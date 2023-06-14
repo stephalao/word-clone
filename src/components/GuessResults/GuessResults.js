621b6c0f4bb0742fc5744d92bf4937a3378a08a2
Stephenson <salao@debt.com>
2023-06-14 18:15:48 -0400

    p4 - Cell

--- src/components/GuessResults/GuessResults.js
+++ src/components/GuessResults/GuessResults.js
@@ -5,11 +5,11 @@ import {NUM_OF_GUESSES_ALLOWED} from '../../constants';
 
 import Guess from "../Guess/Guess";
 
-function GuessResults({guesses}) {
+function GuessResults({ guesses, answer }) {
   return (
     <div className="guess-results">
       {range(NUM_OF_GUESSES_ALLOWED).map((num) => (
-        <Guess key={num} value={guesses[num]}/>
+        <Guess key={num} value={guesses[num]} answer={answer} />
       ))}
     </div>
   );
